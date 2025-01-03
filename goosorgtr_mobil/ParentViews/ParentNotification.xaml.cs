3bfcb0c02d238a1059d52b16be22799eec2d4316
keremcn <33978112+keremcn@users.noreply.github.com>
2025-01-03 17:56:50 +0300

    ss

--- goosorgtr_mobil/ParentViews/ParentNotification.xaml.cs
+++ goosorgtr_mobil/ParentViews/ParentNotification.xaml.cs
@@ -1,8 +1,5 @@
-using goosorgtr_mobil.ViewModels;
 using DevExpress.Maui.CollectionView;
-using DevExpress.Maui.Controls;
-using DevExpress.Maui.Core;
-using System.Globalization;
+using goosorgtr_mobil.ViewModels;
 
 namespace goosorgtr_mobil.ParentViews;
 
@@ -12,51 +9,17 @@ public partial class ParentNotification : ContentPage
 	{
 		InitializeComponent();
 		BindingContext = new NotificationViewModel();
-        filterBottomSheet.State = BottomSheetState.HalfExpanded;
+    
     }
 
-    private void FilterTabHeaderTapped(object sender, ItemHeaderTappedEventArgs e)
-    {
-        UpdateBottomSheetState(e.Index);
-    }
-
-
+    
     void SwipeItem_Tapped(System.Object sender, SwipeItemTapEventArgs e)
     {
         this.collectionView.DeleteItem(e.ItemHandle);
     }
 
-    private void filterTabView_ItemHeaderTapped(object sender, ItemHeaderTappedEventArgs e)
+    private void btnNotificationSetting_Clicked(object sender, EventArgs e)
     {
-        UpdateBottomSheetState(e.Index);
-    }
 
-    void UpdateBottomSheetState(int selectedIndex)
-    {
-        if (selectedIndex == 2)
-            filterBottomSheet.State = BottomSheetState.FullExpanded;
-        else
-            filterBottomSheet.State = BottomSheetState.HalfExpanded;
     }
-}
-public class IsFilterEmptyConverter : IValueConverter
-{
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        if (value is FilterValueInfo filterInfo && filterInfo != null)
-        {
-            return filterInfo.Count == 0;
-        }
-        else if (value is int selectedFilterItems)
-        {
-            return selectedFilterItems == 0;
-        }
-        return true;
-    }
-
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        throw new NotImplementedException();
-    }
-
 }
