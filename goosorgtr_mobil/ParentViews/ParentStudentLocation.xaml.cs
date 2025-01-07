ea0cafec662c310e691a664fab0a615cc1959cee
kaandede <kaandedeofficial@gmail.com>
2025-01-07 16:03:17 +0300

    location+

--- goosorgtr_mobil/ParentViews/ParentStudentLocation.xaml.cs
+++ goosorgtr_mobil/ParentViews/ParentStudentLocation.xaml.cs
@@ -1,7 +1,4 @@
-using Microsoft.Maui.Controls;
 using Microsoft.Maui.Maps;
-using Microsoft.Maui.Devices.Sensors;
-using DevExpress.XtraSpellChecker.Parser;
 using Microsoft.Maui.Controls.Maps;
 
 
@@ -9,13 +6,13 @@ namespace goosorgtr_mobil.ParentViews
 {
     public partial class ParentStudentLocation : ContentPage
     {
+       public List<Pin> Pins { get; set; } = new List<Pin>();
+
         public ParentStudentLocation()
         {
             InitializeComponent();
-
-            //var position = new Location(39.89783152118657, 32.686864328835384); 
+                        //var position = new Location(39.89783152118657, 32.686864328835384); 
             //GoogleMap.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromKilometers(0.1)));
-
         }
 
         protected async override void OnAppearing()
@@ -23,18 +20,40 @@ namespace goosorgtr_mobil.ParentViews
             await Navigation.PopToRootAsync(false);
             base.OnAppearing();
 
-            var position = new Location(39.89783152118657, 32.686864328835384);
-            GoogleMap.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromKilometers(0.1)));
+            var mevkolej = new Location(39.89783152118657, 32.686864328835384);
+            GoogleMap.MoveToRegion(MapSpan.FromCenterAndRadius(mevkolej, Distance.FromKilometers(0.1)));
 
-            var pin = new Pin
+            Pins.Add(new Pin
             {
                 Address = "MEV Koleji",
-                Location = position,
+                Location = mevkolej,
                 Type = PinType.Place,
                 Label = "Öðrenci Þuan Okulda"
-            };
+            });
 
-            GoogleMap.Pins.Add(pin);
+
+            var zaferkolej = new Location(39.8975372596863, 32.67921761534151);
+            GoogleMap.MoveToRegion(MapSpan.FromCenterAndRadius(zaferkolej, Distance.FromKilometers(0.1)));
+
+            Pins.Add(new Pin
+            {
+                Address = "Zafer Koleji",
+                Location = zaferkolej,
+                Type = PinType.Place,
+                Label = "Öðrenci Þuan Okulda"
+            });
+
+
+            //var pin = new Pin
+            //{
+            //    Address = "MEV Koleji",
+            //    Location = position,
+            //    Type = PinType.Place,
+            //    Label = "Öðrenci Þuan Okulda"
+            //};
+
+            GoogleMap.Pins.Clear(); 
+            GoogleMap.ItemsSource = Pins;
         }
        
     }
