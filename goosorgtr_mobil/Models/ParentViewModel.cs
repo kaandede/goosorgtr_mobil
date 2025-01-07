a16afef3f33b5b9abb3320ad2feca1087d8c1b76
Göktuğ <trabzongok@gmail.com>
2025-01-07 15:55:17 +0300

    düzenlemeler eklendi(isim resim değişiklikleri)

--- goosorgtr_mobil/Models/ParentViewModel.cs
+++ goosorgtr_mobil/Models/ParentViewModel.cs
@@ -17,8 +17,8 @@ namespace goosorgtr_mobil.Models
         {
             Profiles.Add(new Profile()
             {
-                Name = "Öğrenci 1",
-                ProfileImage = "back14.png",
+                Name = "Burak YILMAZ",
+                ProfileImage = "https://cdn.pixabay.com/photo/2024/08/14/17/34/ai-generated-8969238_640.jpg",
                 NoPhotos = 3,
                 Konum = "Yemekhane",
                 Descreption = "Öğle Yemeği Molası",
@@ -27,8 +27,8 @@ namespace goosorgtr_mobil.Models
               
             Profiles.Add(new Profile()
             {
-                Name = "Öğrenci 2",
-                ProfileImage = "back13.png",
+                Name = "Kazım YILMAZ",
+                ProfileImage = "https://media.istockphoto.com/id/1148934322/tr/foto%C4%9Fraf/evde-kamera-g%C3%BCl%C3%BCmseyen-g%C3%BCzel-okul-%C3%A7ocuk.jpg?s=170667a&w=0&k=20&c=0avvoMNAUaFdcZ_auXm7VDi1Y6o9ggLTHDsS0qZa_EQ=",
                 NoPhotos = 9,
                 Konum = "Sınıf",
                 Descreption = "Ders İşleniyor",
@@ -36,8 +36,8 @@ namespace goosorgtr_mobil.Models
             });   
             Profiles.Add(new Profile()
             {
-                Name = "Öğrenci 3",
-                ProfileImage = "back3.png",
+                Name = "Ebru Yılmaz",
+                ProfileImage = "https://media.istockphoto.com/id/1148601093/tr/foto%C4%9Fraf/g%C3%BCzel-g%C3%BCl%C3%BCmseyen-sar%C4%B1%C5%9F%C4%B1n-k%C4%B1z-7-8-ya%C5%9F%C4%B1nda-okul-%C3%BCniformal%C4%B1-%C3%A7ocuk-portresi-arka-plan-g%C3%BCne%C5%9Fli.jpg?s=170667a&w=0&k=20&c=fnT76yAe14ET7IiAzMj-pfF0P1iJ95gtUZEmxuh5H_k=",
                 NoPhotos = 11,
                 Konum = "Okula Giriş Yapıldı",
                 Descreption = "Servisten İndi",
