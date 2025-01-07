ed16c99d467492b10901854c0586d32c18f35fab
Göktuğ <trabzongok@gmail.com>
2025-01-07 15:12:11 +0300

    çot

--- goosorgtr_mobil/Models/Announcement.cs
+++ goosorgtr_mobil/Models/Announcement.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace goosorgtr_mobil.Models
+﻿namespace goosorgtr_mobil.Models
 {
     public class Announcement
     {
-        public string Title { get; set; }    // Duyuru başlığı
-        public string Date { get; set; }     // Duyuru tarihi
-        public string Details { get; set; }  // Duyuru detayları
+        public string Title { get; set; }  // Duyuru Başlığı
+        public string Date { get; set; }   // Duyuru Tarihi
+        public string Category { get; set; }  // Kategori (örneğin: Spor, Eğitim)
+        public string Details { get; set; }   // Duyuru Detayları
     }
 }
