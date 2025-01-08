01a06313de52c102403a67743a4233be757e2410
kaandede <kaandedeofficial@gmail.com>
2025-01-08 14:45:33 +0300

    bildiirmler card

--- goosorgtr_mobil/ParentViews/ParentStudentLocation.xaml.cs
+++ goosorgtr_mobil/ParentViews/ParentStudentLocation.xaml.cs
@@ -1,23 +1,30 @@
 using Microsoft.Maui.Maps;
 using Microsoft.Maui.Controls.Maps;
+using goosorgtr_mobil.Models;
+
 
 
 namespace goosorgtr_mobil.ParentViews
 {
     public partial class ParentStudentLocation : ContentPage
     {
-       public List<Pin> Pins { get; set; } = new List<Pin>();
+        public ParentViewModel _parentViewModel;
+
+        public List<Pin> Pins { get; set; } = new List<Pin>();
 
         public ParentStudentLocation()
         {
             InitializeComponent();
-                        //var position = new Location(39.89783152118657, 32.686864328835384); 
+            _parentViewModel = new ParentViewModel();
+            BindingContext = _parentViewModel;
+            //var position = new Location(39.89783152118657, 32.686864328835384); 
             //GoogleMap.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromKilometers(0.1)));
         }
 
         protected async override void OnAppearing()
         {
             await Navigation.PopToRootAsync(false);
+            _parentViewModel.OgrenciGetir();
             base.OnAppearing();
 
            
