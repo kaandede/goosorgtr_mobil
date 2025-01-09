93944de273b5689c165a1e7dd7f8aa1c2528d1be
kaandede <kaandedeofficial@gmail.com>
2025-01-09 15:35:58 +0300

    servis++

--- goosorgtr_mobil/ParentViews/ServisHarita.xaml.cs
+++ goosorgtr_mobil/ParentViews/ServisHarita.xaml.cs
@@ -7,14 +7,14 @@ namespace goosorgtr_mobil.ParentViews
     public partial class ServisHarita : ContentPage
     {
         public ParentViewModel _parentViewModel;
-        private readonly RouteDrawingService _routeService;
+       // private readonly RouteDrawingService _routeService;
         public List<Pin> Pins { get; set; } = new List<Pin>();
         public ServisHarita()
         {
             InitializeComponent();
             _parentViewModel = new ParentViewModel();
             BindingContext = _parentViewModel; 
-            _routeService = new RouteDrawingService("AIzaSyBM9bP45ttREkinQxrnaXijot6bZKLjCn0");
+           // _routeService = new RouteDrawingService("AIzaSyBM9bP45ttREkinQxrnaXijot6bZKLjCn0");
         }
 
 
@@ -24,8 +24,8 @@ namespace goosorgtr_mobil.ParentViews
 
             base.OnAppearing();
 
-            //var mevkolej = new Location(39.89783152118657, 32.686864328835384);
-            //GoogleMap.MoveToRegion(MapSpan.FromCenterAndRadius(mevkolej, Distance.FromKilometers(2)));
+            var mevkolej = new Location(39.89783152118657, 32.686864328835384);
+            GoogleMap.MoveToRegion(MapSpan.FromCenterAndRadius(mevkolej, Distance.FromKilometers(2)));
 
             //Pins.Add(new Pin
             //{
@@ -39,44 +39,44 @@ namespace goosorgtr_mobil.ParentViews
             // GoogleMap.ItemsSource = Pins;
 
 
-            //var ployline1 = new Polyline
-            //{
-            //    StrokeColor = Colors.Yellow,
-            //    StrokeWidth = 15,
-            //    Geopath =
-            //    {
-            //        new Location(39.89783152118657, 32.686864328835384),
-            //        new Location(39.8975372596863, 32.67921761534151)
-            //    }
-            //};
+            var ployline1 = new Polyline
+            {
+                StrokeColor = Colors.Yellow,
+                StrokeWidth = 15,
+                Geopath =
+                {
+                    new Location(39.89783152118657, 32.686864328835384),
+                    new Location(39.8975372596863, 32.67921761534151)
+                }
+            };
 
-            //GoogleMap.MapElements.Add(ployline1);
+            GoogleMap.MapElements.Add(ployline1);
 
-            //var ployline2 = new Polyline
-            //{
-            //    StrokeColor = Colors.Blue,
-            //    StrokeWidth = 15,
-            //    Geopath =
-            //    {
-            //        new Location(39.94587982707268, 32.62854408833028),
-            //        new Location(39.8975372596863, 32.67921761534151)
-            //    }
-            //};
+            var ployline2 = new Polyline
+            {
+                StrokeColor = Colors.Blue,
+                StrokeWidth = 15,
+                Geopath =
+                {
+                    new Location(39.94587982707268, 32.62854408833028),
+                    new Location(39.8975372596863, 32.67921761534151)
+                }
+            };
 
-            //GoogleMap.MapElements.Add(ployline2);
+            GoogleMap.MapElements.Add(ployline2);
 
 
-            await _routeService.DrawRouteOnMap(
-            GoogleMap,
-            new Location(39.89783152118657, 32.686864328835384),
-            new Location(39.8975372596863, 32.67921761534151),
-            Colors.Yellow);
+            //await _routeService.DrawRouteOnMap(
+            //GoogleMap,
+            //new Location(39.89783152118657, 32.686864328835384),
+            //new Location(39.8975372596863, 32.67921761534151),
+            //Colors.Yellow);
 
-            await _routeService.DrawRouteOnMap(
-                GoogleMap,
-                new Location(39.94587982707268, 32.62854408833028),
-                new Location(39.8975372596863, 32.67921761534151),
-                Colors.Blue);
+            //await _routeService.DrawRouteOnMap(
+            //    GoogleMap,
+            //    new Location(39.94587982707268, 32.62854408833028),
+            //    new Location(39.8975372596863, 32.67921761534151),
+            //    Colors.Blue);
 
         }
     }
